9b3d16b4aedf25e8fc7cfd0ec4416f21479ff5a2
Ruslan Tagiev <quiveseek@gmail.com>
2023-05-31 18:43:09 +0300

    feed (posts) page

---
+++ app/api/prompt/route.js
@@ -0,0 +1,19 @@
+import Prompt from '@/models/prompt'
+import { connectToDb } from '@/utils/database'
+
+export const GET = async req => {
+	try {
+
+		await connectToDb()
+		const prompts = await Prompt.find({}).populate('creator')
+
+		return new Response(JSON.stringify(prompts), {
+			status: 200
+		})
+
+	} catch (error) {
+		return new Response('Failed to fetch all posts', {
+			status: 500
+		})
+	}
+}
